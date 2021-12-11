46cbaa0faea3c4fcf681654e9b64f322608b0f58
thynkon <thynkon@protonmail.com>
2021-12-11 18:55:06 +0100

    Parse and print tokens

---
+++ src/main/java/com/craftinginterpreters/jlox/Token.java
@@ -0,0 +1,35 @@
+package com.craftinginterpreters.jlox;
+
+class Token {
+        public TokenType getType() {
+                return type;
+        }
+
+        public String getLexeme() {
+                return lexeme;
+        }
+
+        public Object getLiteral() {
+                return literal;
+        }
+
+        public int getLine() {
+                return line;
+        }
+
+        final TokenType type;
+        final String lexeme;
+        final Object literal;
+        final int line;
+
+        Token(TokenType type, String lexeme, Object literal, int line) {
+                this.type = type;
+                this.lexeme = lexeme;
+                this.literal = literal;
+                this.line = line;
+        }
+
+        public String toString() {
+                return type + " " + lexeme + " " + literal;
+        }
+}
